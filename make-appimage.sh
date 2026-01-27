1ea666fa597d8afada205ab98aa2e132ef919289
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2026-01-27 18:43:28 +0100

    Add GTK deployment and update path mapping
    Added environment variable for GTK deployment and updated path mapping for GTK binaries.

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,6 +11,8 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/scalable/apps/io.github.vikdevelop.SaveDesktop.svg
 export DESKTOP=/usr/share/applications/io.github.vikdevelop.SaveDesktop.desktop
 export DEPLOY_SYS_PYTHON=1
+export DEPLOY_GTK=1
+export GTK_DIR=gtk-4.0
 export PATH_MAPPING='
   /usr/share/savedesktop: ${SHARUN_DIR}/share/savedesktop
   /usr/share/locale:${SHARUN_DIR}/share/locale
@@ -18,8 +20,8 @@ export PATH_MAPPING='
 # Deploy dependencies
 
 quick-sharun \
-          /usr/bin/savedesktop \
-          /usr/bin/gtk4*
+          /usr/bin/savedesktop
+          */usr/bin/gtk4*
 
 # Additional changes can be done in between here
 

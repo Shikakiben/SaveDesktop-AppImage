f4920e5e1b653d13b8f680d30adf46c7e0838d16
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2026-01-27 20:03:49 +0100

    Fix environment variable names for Python and GTK

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,14 +10,12 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/io.github.vikdevelop.SaveDesktop.svg
 export DESKTOP=/usr/share/applications/io.github.vikdevelop.SaveDesktop.desktop
-export DEPLOY_SYS_PYTHON=1
-export DEPLOY_GTK=1
+export DEPLOY_PYTHON=1
 export PATH_MAPPING='
   /usr/share/savedesktop: ${SHARUN_DIR}/share/savedesktop
   /usr/share/locale:${SHARUN_DIR}/share/locale
 '
 # Deploy dependencies
-quick-sharun /usr/bin/savedesktop
 
 DEPLOY_GTK=1 GTK_DIR=gtk-4.0 quick-sharun /usr/bin/savedesktop
 

175d150f16f2ec38f3c013312ab6a920ad58e477
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2026-01-27 17:39:41 +0100

    Define PATH_MAPPING for savedesktop and locale
    Add PATH_MAPPING for shared directories

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,10 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/scalable/apps/io.github.vikdevelop.SaveDesktop.svg
 export DESKTOP=/usr/share/applications/io.github.vikdevelop.SaveDesktop.desktop
 export DEPLOY_SYS_PYTHON=1
-
+export PATH_MAPPING='
+  /usr/share/savedesktop: ${SHARUN_DIR}/share/savedesktop
+  /usr/share/locale:${SHARUN_DIR}/share/locale
+'
 # Deploy dependencies
 
 quick-sharun /usr/bin/savedesktop

86eb4dfa938fdb4883c3627918d34385eed7b887
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2026-01-27 17:49:09 +0100

    Update get-dependencies.sh to install gtk4 and dbus

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,8 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm gtk4 \
+                        dbus
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
